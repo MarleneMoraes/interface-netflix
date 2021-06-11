312a671f8399955e560a66d056b708e6212670b5
Marlene Moraes <marlenevmoraes@outlook.com>
2021-06-11 09:45:55 -0300

    cadastro series)

---
+++ cadastro-series-dotnet/series/Classes/serie.cs
@@ -0,0 +1,13 @@
+namespace series.Classes
+{
+    public class serie:EntidadeBase
+    {
+        //Atributos da série
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        
+    }
+}
